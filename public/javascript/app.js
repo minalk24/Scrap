a7a53f016b20147e74c966399b33bd462da5c660
Minal Kharche <minal.berkeley@gmail.com>
2019-07-22 14:55:07 -0700

    add handlebars to render data from database

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,26 +1,32 @@
-$("#scrape").on("click", function(event) {
+$("#scrape").on("click", function (event) {
     // prevent the page to refresh
     event.preventDefault();
 
     // GET request to scrape the NYT website
     $.ajax("/scrape", {
         method: "GET"
-    }).then(function() {
+    }).then(function () {
         // reload so the articles are displayed
         location.reload();
     });
 });
 
-// Grab the articles as a json
-$.getJSON("/articles", function(data) {
-    // For each one
-    for (var i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
-      $("#news-articles").append("<a href='https://www.nytimes.com" + data[i].url + "' target='_blank'><h4>" + data[i].title + "</h4></a><p class='mb-2'>" + data[i].summary + "</p><button class='btn btn-warning save-button mb-5' type='submit' data-id='" + data[i]._id + "'>Save Article</button><br/>");
-    }
-}); 
+// event listener on the "delete" button to delete the unsaved articles
+$("#delete").on("click", function (event) {
+    // prevent the page to refresh
+    event.preventDefault();
 
-$(document).on("click", ".save-button", function(event) {
+    // post request to delete the articles that haven't been saved
+    $.ajax("/delete-articles", {
+        method: "POST"
+    }).then(function () {
+        // reload the page
+        location.reload();
+    });
+})
+
+//save button
+$(document).on("click", ".save-button", function (event) {
     // prevent the page to refresh
     event.preventDefault();
 
@@ -31,10 +37,27 @@ $(document).on("click", ".save-button", function(event) {
     // put request to update the value of "save" in the Article collection
     $.ajax("/save-article/" + articleID, {
         method: "POST"
-    }).then(function() {
+    }).then(function () {
         // reload so the articles are displayed
         location.reload();
     });
 
+});
 
+//  "delete-saved-article" button to delete the saved articles
+$(document).on("click", ".delete-saved-article", function (event) {
+    // prevent the page to refresh
+    event.preventDefault();
+
+    // grab the id of the article whose button has been clicked
+    var articleID = $(this).data("id");
+    // console.log(articleID);
+
+    // post request to delete the saved article whose "delete" button has been clicked
+    $.ajax("/delete-article/" + articleID, {
+        method: "POST"
+    }).then(function () {
+        // reload the page
+        location.reload();
+    });
 });
--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-
-
 // =============================
 // Dependencies
 // =============================
@@ -21,11 +19,11 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 // load the MongoDB models
-var db = require("/models");
+var db = require("./models");
 
 // =============================
 // Configuration of the Express app
-/// =============================
+// =============================
 
 // create an Express app
 var app = express();
@@ -42,8 +40,12 @@ app.use(express.json());
 // by generating a route itself for everything within the "public" folder
 app.use(express.static("public"));
 
-// =============================
+
+// ---------------------------------------------------
 // Set handlebars as the default templating engine
+// ---------------------------------------------------
+// =============================
+// Dependencies
 // =============================
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -52,91 +54,174 @@ app.set("view engine", "handlebars");
 // =============================
 // Connect to the MongoDB
 // =============================
+
 // If deployed, use the deployed database. Otherwise use the local database called "newsArticles"
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsArticles";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+ 
 // =============================
 // Routes
 // =============================
 
-// define the route to display the home page
-app.get("/", function (req, res) {
-    res.render("index");
+// define the route to display the "home" page
+app.get("/", function(req, res) {
+    db.Article.find({saved: false})
+        .then(function(dbArticle) {
+            var hbsObj = {
+                data: dbArticle
+            }
+            console.log(hbsObj);
+            // If we were able to successfully find Articles
+            // render the page with the data
+            res.render("index", hbsObj);
+        })
+        .catch(function(err) {
+            // If an error occurred, log it
+            console.log(err);
+        })
+});
+
+// define the route to display the "saved articles" page
+app.get("/saved-articles", function(req, res) {
+    db.Article.find({saved:true})
+        .then(function(dbArticle) {
+            var hbsObj = {
+                data: dbArticle
+            }
+            console.log(hbsObj);
+            // If we were able to successfully find Articles
+            // render the page with the data
+            res.render("saved-articles", hbsObj);
+        })
+        .catch(function(err) {
+            // If an error occurred, log it
+            console.log(err);
+        })
 });
 
 // define the route to scrape NYT website
-app.get("/scrape", function (req, res) {
-    //delete all that in the "Article" 
-    db.Article.deleteMany({});
+app.get("/scrape", function(req, res) {
     // grab the body of the html with axios
-    axios.get("https://www.nytimes.com/").then(function (response) {
+    axios.get("https://www.nytimes.com/").then(function(response) {
         // load that into cheerio and save it to $ for a shorthand selector
         var $ = cheerio.load(response.data);
-
+  
         // grab every a tag within article tag, and do the following:
-        $("article a").each(function (i, element) {
+        $("article a").each(function(i, element) {
             // Save an empty result object
             var result = {};
-
-            // Add the text and href of every link, and save them as properties of the result object
+  
             // Add the title and href of every link + the summary of every article 
             // and save them as properties of the result object
             result.title = $(this)
-                .children("a")
                 .children("div")
                 .children("h2")
                 .text();
-            result.link = $(this)
-                .children("a")
             result.url = $(this)
                 .attr("href");
             result.summary = $(this)
                 .children("p")
                 .text();
-
-            // Create a new Article using the `result` object built from scraping
+  
+            // Create new documents within the "Article" collection using the `result` object built from scraping
             db.Article.create(result)
-            app.get("/scrape", function (req, res) {
-                console.log(err);
-            })
+                .then(function(dbArticle) {
+                    // View the added result in the console
+                    console.log(dbArticle);
+                })
+                .catch(function(err) {
+                    // If an error occurred, log it
+                    console.log(err);
+                })
         });
-
-        // Send a message to the client
-        res.send("Scrape Complete");
-        // close the connection
-        res.end();
+        // redirect to the root route
+        res.redirect("/");
     });
 });
 
-// define route to get all the articles from the db
-app.get("/articles", function (req, res) {
-    // Grab every document in the Article collection
-    db.Article.find({})
-        .then(function (dbArticle) {
-            // If we were able to successfully find Articles, send them back to the client
+// define the route to delete all the articles that haven't been saved
+app.post("/delete-articles", function(req, res) {
+    // delete many - all the unsaved article
+    db.Article.deleteMany({saved: false})
+        .then(function(dbArticle) {
+            // View the updated result in the console
+            console.log(dbArticle);
+        })
+        .catch(function(err) {
+            // If an error occurred, log it
+            console.log(err);
+        })
+
+    // redirect to the root route
+    res.redirect("/");
+});
+
+// define the route to save an article when its "save" button has been clicked
+app.post("/save-article/:id", function(req, res) {
+    // find and update the article corresponding to the id
+    db.Article.findOneAndUpdate({ _id: req.params.id }, { saved: true }, { new: true })
+        .then(function(dbArticle) {
+            // View the updated result in the console
+            console.log(dbArticle);
+        })
+        .catch(function(err) {
+            // If an error occurred, log it
+            console.log(err);
+        })
+
+    // end the connection
+    res.end();
+});
+
+// define the route to delete a saved article
+app.post("/delete-article/:id", function(req, res) {
+    // delete one - the saved article whose "delete-save-article" button has been clicked
+    db.Article.deleteOne({ _id: req.params.id })
+        .then(function(dbArticle) {
+            // View the updated result in the console
+            console.log(dbArticle);
+        })
+        .catch(function(err) {
+            // If an error occurred, log it
+            console.log(err);
+        })
+
+    // end the connection
+    res.end();
+});
+
+// define the route to populate a saved article with its note
+app.get("/note-article/:id", function(req, res) {
+    // find one - the saved article whose "note" button has been clicked
+    db.Article.findOne({ _id: req.params.id })
+        // populate all of the notes associated with it
+        .populate("notes")
+        .then(function(dbArticle) {
+            // If we were able to successfully find an Article with the given id, send it back to the client
             res.json(dbArticle);
         })
-        .catch(function (err) {
+        .catch(function(err) {
             // If an error occurred, send it to the client
             res.json(err);
         });
 });
 
-// define the route to save article when "save" button clicked
-app.post("/save-article/:id", function (req, res) {
-    // find and update the article corresponding to the id
-    db.Article.findOneAndUpdate({ _id: req.params.id }, { saved: true }, { new: true })
-        .then(function (dbArticle) {
-            // View the added result in the console
-            console.log(dbArticle);
+// define the route to save/update the note for the corresponding article
+app.post("/note-article/:id", function(req, res) {
+    // Create a new note with the user input
+    db.Note.create(req.body)
+        .then(function(dbNote) {
+            // once the new note has been created, find the associate article and update its notes with the note just created
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: { notes: dbNote._id } }, { new: true });
         })
-        .catch(function (err) {
-            // If an error occurred, log it
-            console.log(err);
+        .then(function(dbArticle) {
+            // If we were able to successfully update an Article, send it back to the client
+            res.json(dbArticle);
         })
-    // close the connection
-    res.end();
+        .catch(function(err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
 });
 
 // =============================
@@ -144,6 +229,6 @@ app.post("/save-article/:id", function (req, res) {
 // =============================
 
 // so that it can begin listening to client requests.
-app.listen(PORT, function () {
+app.listen(PORT, function() {
     console.log("App listening on: http://localhost:" + PORT);
-}); 
+});
